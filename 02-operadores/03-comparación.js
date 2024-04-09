2fd7831fd186c1cbcb11b6b21f808150440a61df
Neto Cruz <galaxy.net0710@gmail.com>
2024-04-09 15:24:54 -0600

    next lessons

---
+++ 02-operadores/03-comparación.js
@@ -0,0 +1,14 @@
+let a = 10
+//operadores relacionales
+console.log(a>5)
+console.log(a>=5)
+
+console.log(a<5)
+console.log(a<=5)
+
+//operadores comparación
+console.log(a==10)
+console.log(a!=10)
+console.log(a=='10')//true solo compara valor
+console.log(a==='10')//false compara valor y tipo(string, number...)
+console.log(a!=='10')
---
+++ 02-operadores/04-logicos.js
@@ -0,0 +1,15 @@
+// AND, OR y NOT
+
+// and &&
+console.log(true && true)
+console.log(false && true)
+
+// or ||
+console.log(true || true)
+console.log(true || false)
+
+// not !
+
+let mayorEdad = true
+
+console.log(!mayorEdad)
