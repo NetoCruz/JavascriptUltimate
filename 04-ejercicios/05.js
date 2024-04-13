628fd9a9e2cbe863f6a527afce4596d1205726ef
Neto Cruz <galaxy.net0710@gmail.com>
2024-04-13 14:57:42 -0600

    new excercise

---
+++ 04-ejercicios/05.js
@@ -0,0 +1,22 @@
+
+let array=[80,5,7,15,2,100,55]
+let i=0
+let newArray=[]
+// let minus = 0
+
+
+ while(i<array.length){
+
+      let minus = array[0]
+      if(minus<array[i+1]){
+         //  array[i] == array[i+1]
+        
+     
+            console.log(array[i]) 
+          
+        } //else{ array[i] == array[i+1]}
+     i++
+     //console.log(newArray) 
+   
+
+ }
