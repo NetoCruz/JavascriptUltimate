ce0e743ad41324bfae862b442cf6d96fe88b1518
Neto Cruz <galaxy.net0710@gmail.com>
2024-04-15 15:12:50 -0600

    fizzbuzz fixed

--- retosJavascript/fizzbuzz.js
+++ retosJavascript/fizzbuzz.js
@@ -5,18 +5,31 @@
 //    Múltiplos de 5 por la palabra "buzz".
 //    Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
 
-let i=0
-  while(i<=50){
-      if(i%3==0){
-        console.log('fizz', i) 
-      }
-      if(i%5==0){
-         console.log('buzz',i) 
-       }if(i%3==0 && i%5==0){
+let i=1
+  while(i<=100){
+      // if(i%3==0){
+      //   console.log('fizz', i) 
+      // }
+      // if(i%5==0){
+      //    console.log('buzz',i) 
+      //  }if(i%3==0 && i%5==0){
+      //   console.log('fizzbuzz',i)
+      //  }else {
+      //   console.log(i) 
+      //  }
+
+      if(i%3==0 && i%5==0){
         console.log('fizzbuzz',i)
        }else {
-        console.log(i) 
+          if(i%3==0){
+                  console.log('fizz', i) 
+                }
+                if(i%5==0){
+                  console.log('buzz',i) 
+                }
        }
+       
+      
      
    i++
   }
