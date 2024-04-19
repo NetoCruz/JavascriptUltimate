392183bb71a982557c186aa198ed2f75372ba943
Neto Cruz <galaxy.net0710@gmail.com>
2024-04-19 17:04:50 -0600

    Prime number

---
+++ retosJavascript/Numeroprimo.js
@@ -0,0 +1,61 @@
+
+
+//function obtenerNumeroPrimo(){
+let i=0
+let numbers=[2,3,4,5,6,7,8,9,10]
+let result=[]
+let operator=[]
+    // for(i=2; i<=10; i++){
+    //     numbers.push(i)
+    // }
+  
+    // for(i=2; i<=10;i++){
+    //  numbers.push(i)
+    //  } 
+    //  numbers.sort(function (b, a) {
+    //     return a -b;
+    //   })  
+             
+            //  for(i=0; i<=numbers.length-1;i++){
+            //      let re = numbers[0]%numbers[i+1]
+            //  result.push(re)
+            //     }  
+
+            // function isPrime(num) {
+            //     for (let i = 2; num > i; i++) {
+            //       if (num % i === 0) {
+            //         return false;
+            //       }
+            //     }
+            //     return num > 1;
+            //   }
+            //  console.log(numbers.filter(isPrime));
+    
+         //numbers.forEach(function (element) {
+                
+                 
+                    
+              
+           
+               
+    
+    // let re = numbers[0]%numbers[i+1]
+    // result.push(re)
+       
+      
+    // console.log(result) 
+        // let num=0
+        // for (num=i;num<i-1;num++){
+        //     if(num%num==0){
+        //         console.log('hola')
+        //     }
+        // }
+       
+    //  if(i/i==1 && i/1==i){
+    //     console.log(i)
+    //   }
+  
+ // console.log(numbers)
+//}
+
+ 
