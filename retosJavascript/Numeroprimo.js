c7e9eb7a8dc786f9a60770ab32afeaf4d014ec3d
Neto Cruz <galaxy.net0710@gmail.com>
2024-04-19 18:31:31 -0600

    try it

--- retosJavascript/Numeroprimo.js
+++ retosJavascript/Numeroprimo.js
@@ -1,13 +1,29 @@
 
 
 //function obtenerNumeroPrimo(){
-let i=0
+
 let numbers=[2,3,4,5,6,7,8,9,10]
 let result=[]
 let operator=[]
     // for(i=2; i<=10; i++){
     //     numbers.push(i)
     // }
+
+    numbers.forEach(element => {
+             for (let i = 0; i<numbers.length-1; i++) {
+                let re =element % numbers[i+1]
+                result.push(re)
+                //console.log(element,re)
+                  // if (element % numbers[i] === 0) {
+                  //  console.log(element,'numero natural')
+                  // }
+                }
+                
+    });
+//     const resultados = {}
+// for (const el of result) resultados[el] = resultados[el] + 1 || 1
+
+// console.log(resultados)
   
     // for(i=2; i<=10;i++){
     //  numbers.push(i)
@@ -39,23 +55,5 @@ let operator=[]
            
                
     
-    // let re = numbers[0]%numbers[i+1]
-    // result.push(re)
-       
-      
-    // console.log(result) 
-        // let num=0
-        // for (num=i;num<i-1;num++){
-        //     if(num%num==0){
-        //         console.log('hola')
-        //     }
-        // }
-       
-    //  if(i/i==1 && i/1==i){
-    //     console.log(i)
-    //   }
-  
- // console.log(numbers)
-//}
-
+   
  
