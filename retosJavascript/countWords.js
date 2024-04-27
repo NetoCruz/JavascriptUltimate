e832e365be15dc054b4cf9a395d8fe9b9e9d9acc
Neto Cruz <galaxy.net0710@gmail.com>
2024-04-27 16:50:06 -0600

    countword advance

--- retosJavascript/countWords.js
+++ retosJavascript/countWords.js
@@ -1,9 +1,21 @@
 
 
 let words='La jirafa es la mejor amiga'
-let string=['la','jirafa','lola','es','la','mejor','jirafa']
-
-
-
-    
+let string=['la','jirafa','lola','es','la','mejor','jirafa','la']
+let count=0
+let newArr=[]
+for (let i = 0; i < string.length; i++) {
+    let letter=0
+    for (letter = 0; letter < string.length; letter++) {
+        
+        if(string[letter]===string[i+1]){
+         newArr.push(string[letter])
+    //console.log('coincide', count)
+   }
+    }
+   
+    //letter++
+}
+console.log(newArr) 
+ 
 
